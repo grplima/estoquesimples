d5bab61b8dac03036c44830ffc9113dc3a3ce10e
Geraldo Rondinele Pereira Lima <geraldo.lima@proway.treina>
2024-02-07 21:45:18 -0300

    tela de cadastro concluida primeira etapa

--- lib/cadastroNovoProduto.dart
+++ lib/cadastroNovoProduto.dart
@@ -9,37 +9,87 @@ void main() {
 class cadastronovo extends StatelessWidget {
   cadastronovo({super.key});
 
-  final _controller = TextEditingController();
+  final _controllergtin = TextEditingController();
+  final _controllerdescricao = TextEditingController();
+
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cadastro"),
+        title: Text("Cadastro de novo produto"),
       ),
-      body: Column(children: [
-        Row(children: [
-          Expanded(
-            child: TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                labelText: 'Codigo GTIN',
-                border: OutlineInputBorder(), // InputBorder.none
-                enabledBorder: OutlineInputBorder(borderSide: BorderSide()),
-                labelStyle: TextStyle(
-                  fontSize: 40,
-                  color: Color(0xff00d7f3),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xff00d7f3), width: 2),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(children: [
+          Row(children: [
+            Container(
+              child: Expanded(
+                child: TextField(
+                  controller: _controllergtin,
+                  decoration: const InputDecoration(
+                    labelText: 'Codigo GTIN',
+                    border: OutlineInputBorder(),
+                    // InputBorder.none
+                    enabledBorder: OutlineInputBorder(borderSide: BorderSide()),
+                    labelStyle: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black54,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey, width: 2),
+                    ),
+                  ),
                 ),
               ),
             ),
+            Container(
+              child:
+                  IconButton(onPressed: () {}, icon: Icon(Icons.photo_camera)),
+            )
+          ]),
+          SizedBox(
+            height: 14,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                    controller: _controllerdescricao,
+                    decoration: InputDecoration(
+                      labelText: 'Descrição do produto',
+                      border: OutlineInputBorder(),
+                      // InputBorder.none
+                      enabledBorder:
+                          OutlineInputBorder(borderSide: BorderSide()),
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black54,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blueGrey, width: 2),
+                      ),
+                    )),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 14,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Salvar Produto'),
+              ))
+            ],
           )
         ]),
-        Row(),
-        Row()
-      ]),
+      ),
     );
   }
 }
