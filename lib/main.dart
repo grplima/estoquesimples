283768101a00efe2c1c6aeec0a27abe6248327a1
Thiago-Souzaa <tssti@outlook.com>
2024-02-11 20:00:11 -0300

    inclusao da tela inicial e barras na parte de baixo

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,248 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: TelaInicial(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class TelaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Tela Inicial",
+          style: TextStyle(
+            color: Color(0xFFEDC71F), // Cor #EDC71F
+          ),
+        ),
+        centerTitle: true, // Centraliza o título
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/Clear_Sans.png'), // Caminho relativo à pasta assets
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Cadastrar Produto',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TelaInicial()),
+            );
+          } else if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Cadastronovo()),
+            );
+          }
+        },
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Cadastronovo extends StatelessWidget {
+  Cadastronovo({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _controllergtin = TextEditingController();
+  final _controllerdescricao = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          "Cadastro de novo produto",
+          style: TextStyle(
+            color: Color(0xFFEDC71F), // Cor #EDC71F
+          ),
+        ),
+        centerTitle: true, // Centraliza o título
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Retorna para a tela anterior (TelaInicial)
+          },
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.center, // Centralizar verticalmente
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controllergtin,
+                    decoration: const InputDecoration(
+                      labelText: 'Codigo GTIN',
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide()),
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black54,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blueGrey, width: 2),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.photo_camera),
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(
+              height: 14,
             ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controllerdescricao,
+                    decoration: InputDecoration(
+                      labelText: 'Descrição do produto',
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide()),
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black54,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blueGrey, width: 2),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Instanciar ProdutoDatabase para acessar os métodos de banco de dados
+                      final produtoDatabase = ProdutoDatabase();
+
+                      // Obter os valores inseridos pelo usuário
+                      final gtin = _controllergtin.text;
+                      final descricao = _controllerdescricao.text;
+
+                      // Criar um novo Produto
+                      final novoProduto = Produto(gtin: gtin, descricao: descricao);
+
+                      // Chamar o método de inserção do ProdutoDatabase
+                      produtoDatabase.insert(novoProduto);
+
+                      // Limpar os campos de texto após a inserção
+                      _controllergtin.clear();
+                      _controllerdescricao.clear();
+
+                      // Exibir um snackbar informando que o produto foi salvo
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Produto salvo com sucesso!'),
+                        ),
+                      );
+                    },
+                    child: Text('Salvar Produto'),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Produto {
+  final int? id;
+  final String gtin;
+  final String descricao;
+
+  Produto({this.id, required this.gtin, required this.descricao});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'produto_id': id,
+      'gtin': gtin,
+      'descricao': descricao,
+    };
+  }
+
+  factory Produto.fromMap(Map<String, dynamic> map) {
+    return Produto(
+      id: map['produto_id'],
+      gtin: map['gtin'],
+      descricao: map['descricao'],
+    );
+  }
+}
+
+class ProdutoDatabase {
+  static const String _tableName = 'produto';
+  static const String _id = 'produto_id';
+  static const String _gtin = 'gtin';
+  static const String _descricao = 'descricao';
+
+  static const String createTableSQL =
+      'CREATE TABLE $_tableName ($_id INTEGER PRIMARY KEY AUTOINCREMENT, $_gtin TEXT, $_descricao TEXT)';
+
+  Future<Database> _getDatabase() async {
+    final String path = join(await getDatabasesPath(), 'Relatorios.db');
+    return openDatabase(path, onCreate: (db, version) async {
+      await db.execute(createTableSQL);
+      await _insertSampleData(db);
+    }, version: 1);
+  }
+
+  Future<void> init() async {
+    final Database db = await _getDatabase();
+    await db.execute(createTableSQL);
+    await _insertSampleData(db);
+  }
+
+  Future<void> _insertSampleData(Database db) async {
+    await db.execute(
+        "INSERT INTO $_tableName ($_gtin, $_descricao) VALUES ('1234567890123', 'Produto A')");
+    await db.execute(
+        "INSERT INTO $_tableName ($_gtin, $_descricao) VALUES ('9876543210987', 'Produto B')");
+    // Add more insert statements for other sample data
+  }
+
+  Future<void> insert(Produto produto) async {
+    final Database db = await _getDatabase();
+    await db.insert(
+      _tableName,
+      produto.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 }
